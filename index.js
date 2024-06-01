84cfbe8ac373106095979ff61897b36414234469
Faris Shah <132473241+syedfariskarachi@users.noreply.github.com>
2024-06-01 10:31:13 +0500

    DoWhile_loop

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 const random_number = Math.floor(Math.random() * 10 + 1);
-const answer = await inquirer.prompt([
-    {
-        name: "user_guess_number",
-        type: "number",
-        message: "Please Guess a number Between 1-10"
-    },
-]);
-if (answer.user_guess_number == random_number) {
-    console.log("Congratulation you win");
+async function game() {
+    let playAgain;
+    do {
+        const answer = await inquirer.prompt([
+            {
+                name: "user_guess_number",
+                type: "number",
+                message: "Please Guess a number Between 1-10"
+            },
+        ]);
+        if (answer.user_guess_number == random_number) {
+            console.log("Congratulation you win");
+        }
+        else {
+            console.log("You lose (Right number is = " + random_number + ")");
+        }
+        let again = await inquirer.prompt({
+            message: "would you like to play again",
+            type: "list",
+            name: "play_again",
+            choices: ["Yes", "No"]
+        });
+        playAgain = again.play_again == "Yes";
+    } while (playAgain);
 }
-else {
-    console.log("You lose (Right number is = " + random_number + ")");
-}
+game();
 // console.log(answer)
--- index.ts
+++ index.ts
@@ -1,17 +1,33 @@
 #! /usr/bin/env node
 import inquirer from "inquirer"
 const random_number = Math.floor(Math.random()*10+1);
-const answer = await inquirer.prompt([
-  {
-    name:"user_guess_number",
+async function game() {
+  let play_again;  
+  
+  do {
+  
+  const answer = await inquirer.prompt([
+    {
+      name:"user_guess_number",
     type:"number",
     message:"Please Guess a number Between 1-10"
   },
 ])
-if(answer.user_guess_number==random_number){
-    console.log("Congratulation you win")
+
+  if(answer.user_guess_number==random_number){
+      console.log("Congratulation you win")
+  }
+  else{
+      console.log("You lose (Right number is = "+random_number+")")
+  }
+  let again=await inquirer.prompt({
+    message:"would you like to play again",
+    type:"list",
+    name:"play_again",
+    choices:["Yes","No"]
+  })
+  play_again=again.play_again=="Yes"
+} while (play_again);
 }
-else{
-    console.log("You lose (Right number is = "+random_number+")")
-}
+game()
 // console.log(answer)
